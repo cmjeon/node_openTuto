2674d17f239c92b914eb20fc3905a6748578aeac
cmj <chmin82@gmail.com>
2020-02-15 22:24:15 +0900

    24 글목록 출력하기

--- main.js
+++ main.js
@@ -6,11 +6,20 @@ var app = http.createServer(function (request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    if(pathname === '/') {
-        if(queryData.id == undefined) {
-            title = 'Welcome';
-            description = 'Hello, Node.js';
-            var template = `
+    if (pathname === '/') {
+        if (queryData.id == undefined) {
+            fs.readdir('./data', function (err, filelist) {
+                console.log(filelist);
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var list = '<ul>';
+                var i = 0;
+                while (i < filelist.length) {
+                    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+                    i = i + 1;
+                }
+                list = list + '</ul>';
+                var template = `
                 <!doctype html>
                 <html>
                 <head>
@@ -19,23 +28,31 @@ var app = http.createServer(function (request, response) {
                 </head>
                 <body>
                 <h1><a href="/">WEB</a></h1>
-                <ol>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=Javascript">JavaScript</a></li>
-                </ol>
+                ${list}
                 <h2>${title}</h2>
                 <p>${description}
                 </p>
                 </body>
                 </html>
                 `;
-            response.writeHead(200);
-            response.end(template);
+                response.writeHead(200);
+                response.end(template);
+            });
         } else {
-            fs.readFile(`data/${queryData.id}`, 'utf-8', function (err, description) {
-                var title = queryData.id;
-                var template = `
+            fs.readdir('./data', function (err, filelist) {
+                console.log(filelist);
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var list = '<ul>';
+                var i = 0;
+                while (i < filelist.length) {
+                    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+                    i = i + 1;
+                }
+                list = list + '</ul>';
+                fs.readFile(`data/${queryData.id}`, 'utf-8', function (err, description) {
+                    var title = queryData.id;
+                    var template = `
                     <!doctype html>
                     <html>
                     <head>
@@ -44,19 +61,16 @@ var app = http.createServer(function (request, response) {
                     </head>
                     <body>
                     <h1><a href="/">WEB</a></h1>
-                    <ol>
-                        <li><a href="/?id=HTML">HTML</a></li>
-                        <li><a href="/?id=CSS">CSS</a></li>
-                        <li><a href="/?id=Javascript">JavaScript</a></li>
-                    </ol>
+                    ${list}
                     <h2>${title}</h2>
                     <p>${description}
                     </p>
                     </body>
                     </html>
                     `;
-                response.writeHead(200);
-                response.end(template);
+                    response.writeHead(200);
+                    response.end(template);
+                });
             });
         }
     } else {
