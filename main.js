dff3fc13efdbc776d7d13e826c65237031ff3af9
cmj <chmin82@gmail.com>
2020-02-17 22:07:41 +0900

    33 App제작 파일생성과 리다이렉션

--- main.js
+++ main.js
@@ -77,19 +77,20 @@ var app = http.createServer(function (request, response) {
             response.writeHead(200);
             response.end(template);
         });
-    } else if(pathname === '/create_process') {
+    } else if (pathname === '/create_process') {
         var body = '';
-        request.on('data', function(data){
+        request.on('data', function (data) {
             body = body + data;
         });
-        request.on('end', function(){
+        request.on('end', function () {
             var post = qs.parse(body);
             var title = post.title;
             var description = post.description;
-            console.log(post);
+            fs.writeFile(`data/${title}`, description, 'utf-8', function (err) {
+                response.writeHead(302, {Location: `/?id=${title}`});
+                response.end('Success');
+            });
         });
-        response.writeHead(404);
-        response.end('Success');
     } else {
         response.writeHead(404);
         response.end('Not found');
